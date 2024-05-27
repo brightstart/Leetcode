c46ccaec3afd7741ba03c9249780868db69cb1e7
Prateek Arora <aroraprateek091@outlook.com>
2024-05-27 17:58:06 +0530

    Logic with Arrays sort

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public boolean identifyAnagrams(String str1, String str2){
+        if(str1.equals(str2))
+            return true;
+        if(str1.length() != str2.length())
+            return false;
+
+        char[] array1 = str1.toLowerCase().toCharArray();
+        Arrays.sort(array1);
+        char[] array2 = str2.toLowerCase().toCharArray();
+        Arrays.sort(array2);
+
+        return (Arrays.compare(array1, array2) == 0);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Listen & silence");
+        Main m = new Main();
+        System.out.println(m.identifyAnagrams("Listen", "silence"));
+
+        System.out.println("Listen & silent");
+        System.out.println(m.identifyAnagrams("Listen", "silent"));
+    }
+}
